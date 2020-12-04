633615495c98a200c54f8cb91309602885d84bca
JaySan93 <75321700+JaySan93@users.noreply.github.com>
2020-12-04 15:47:50 +0700

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -109,10 +109,7 @@ except:
 trackid = subprocess.Popen("curl 192.168.0.187/api/v1/getstate", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 (outputRAW, error) = trackid.communicate()
 if trackid.returncode != 1: #if online
-   trackname = outputRAW.decode().split(',')[3]
-   artist = outputRAW.decode().split(',')[4]
-   trackIDString = (str(artist)+str(' - ')+str(trackname))
-   #albumart = outputRAW.decode().split('\"')[21] #das waere sau cool
+trackIDString = 'MOBIUS DAEBAK'
 else:
    artist = ' '
    title = ' '
