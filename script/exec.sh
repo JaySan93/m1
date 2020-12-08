8ee23b0bdb5c32c8cabe40eaaaf7d132f9a61bb7
JaySan93 <75321700+JaySan93@users.noreply.github.com>
2020-12-08 13:09:57 +0700

    Update exec.sh

--- script/exec.sh
+++ script/exec.sh
@@ -5,5 +5,5 @@
 # * * * * * /home/pi/script/waveshareEpaper/script/exec.sh >> /home/pi/script/waveshareEpaper/clocklog2.txt 2>&1
 
 export DISPLAY=:0.0
-source /home/pi/.profile
-python3 /home/pi/m1/script/waveshare_uhr3.py
+source /home/volumio/.profile
+python3 /home/volumio/m1/script/waveshare_uhr3.py
