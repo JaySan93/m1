8b3903b662d875f53aad1d1579d950e794166159
JaySan93 <75321700+JaySan93@users.noreply.github.com>
2020-12-28 23:02:03 +0700

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -131,13 +131,13 @@ def main():
         #draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
         draw.text((0, 66), trackIDString, font = fontM, fill = 0)       # volumio track ID
         draw.text((0, 82), title, font = fontM, fill = 0)       # volumio track ID
-        draw.text((180, 24), bitrate, font = fontXS, fill = 0)
-        draw.text((180, 12), streamingservice, font = fontXS, fill = 0)
-        draw.text((180, 36), volume, font = fontM, fill = 0)
+        draw.text((140, 24), bitrate, font = fontXS, fill = 0)
+        draw.text((140, 12), streamingservice, font = fontXS, fill = 0)
+        draw.text((180, 36), volume, font = fontXS, fill = 0)
         #draw.line((0, 77, 264, 77), fill = 0)
         draw.text((0, 18), Uhrzeit, font = fontXXL, fill = 0)           # time
         draw.text((195, 0), str(t),font = fontXS, fill = 0)             #cpu temp   
-        draw.text((120, 0), 'CPU TEMP',font = fontXS, fill = 0)
+        draw.text((130, 0), 'CPU TEMP',font = fontXS, fill = 0)
         draw.text((140, 36), 'VOLUME',font = fontXS, fill = 0)
         
         #Update display
